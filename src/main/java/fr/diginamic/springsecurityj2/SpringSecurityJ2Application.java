e221c345ef40bf4dab001714d2e05a4cdb49a1ea
Sandrine ALCAZAR <33418628+Vanda89@users.noreply.github.com>
2025-08-28 10:42:14 +0200

    first commit

---
+++ src/main/java/fr/diginamic/springsecurityj2/SpringSecurityJ2Application.java
@@ -0,0 +1,13 @@
+package fr.diginamic.springsecurityj2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityJ2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityJ2Application.class, args);
+	}
+
+}
---
+++ src/test/java/fr/diginamic/springsecurityj2/SpringSecurityJ2ApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.diginamic.springsecurityj2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJ2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
